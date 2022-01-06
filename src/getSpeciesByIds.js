750e121ed2130c5b9403f3b22b22faa74e3dde62
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-01-06 17:48:34 -0300

    Requisito 1,iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  // console.log(data)
+  if (!ids) {
+    return [];
+  }
+  const specie = ids.map((element) =>
+    data.species.find((elementId) => elementId.id === element));
+  return specie;
 }
-
+console.log(getSpeciesByIds(
+  '0938aa23-f153-4937-9f88-4858b24d6bce',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46',
+));
 module.exports = getSpeciesByIds;

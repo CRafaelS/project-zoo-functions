21fa7ef4f0ce92c83e735eddeea740eddc80a645
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-01-06 18:48:25 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalsOlder = data.species
+    .filter(({ name }) => name === animal);
+  const ageAnimal = animalsOlder[0].residents.every((num) => num.age > age);
+  return ageAnimal;
 }
-
 module.exports = getAnimalsOlderThan;

ed46dfc48d72b89f80e9c726cda029074bb931b8
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-01-06 19:01:21 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const findName = data.employees
+    .find((nam) => nam.firstName === employeeName || nam.lastName === employeeName);
+  return findName;
 }
-
 module.exports = getEmployeeByName;

c0f64f3ff39904269a0a6876efd96f4775d6e548
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-01-10 18:21:56 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,28 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const numEntrants = entrants.reduce((acc, compareAge) => {
+    if (compareAge.age < 18) {
+      acc.child += 1;
+    } else if (compareAge.age < 50) {
+      acc.adult += 1;
+    } else {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
+  return numEntrants;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+  const numEntrant = countEntrants(entrants);
+  const price = numEntrant.child * data.prices.child
+      + numEntrant.adult * data.prices.adult
+      + numEntrant.senior * data.prices.senior;
+  return price;
 }
-
 module.exports = { calculateEntry, countEntrants };

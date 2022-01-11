a5789f29defb4f38e4017c0dff39defd82d238c0
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-01-10 21:45:29 -0300

    Requisito 9 refatorado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,9 +5,8 @@ function getOldestFromFirstSpecies(id) {
   const employeeId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
 
   const animal = data.species.find((findAnimal) => findAnimal.id === employeeId).residents
-    .reduce((acc, elem) => (acc.age > elem.age ? acc : elem));
+    .sort((a, b) => b.age - a.age)[0];
   return Object.values(animal);
-  //   .find((listId) => listId.id === list))//.reduce((acc, elem) => (acc.age > elem.age ? acc : elem));
 }
 
 module.exports = getOldestFromFirstSpecies;

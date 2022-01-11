ebd96319e4431c6c51e5ceacd3a3c8b3493f7a78
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-01-10 21:07:36 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employeeId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+
+  const animal = data.species.find((findAnimal) => findAnimal.id === employeeId).residents
+    .reduce((acc, elem) => (acc.age > elem.age ? acc : elem));
+  return Object.values(animal);
+  //   .find((listId) => listId.id === list))//.reduce((acc, elem) => (acc.age > elem.age ? acc : elem));
 }
 
 module.exports = getOldestFromFirstSpecies;

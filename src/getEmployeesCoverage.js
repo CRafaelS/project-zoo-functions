6ea2a1d26dbc11159867c748ea868812d09c9465
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2022-01-12 19:13:00 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const employeeLocation = () => {
+  const allresult = data.employees
+    .reduce((acc, { id, firstName, lastName, responsibleFor }) => {
+      acc.push({
+        id,
+        fullName: `${firstName} ${lastName}`,
+        species: responsibleFor.map((animal) => data.species
+          .find((test) => test.id === animal).name),
+        locations: responsibleFor.map((animal) => data.species
+          .find((test) => test.id === animal).location),
+      });
+      return acc;
+    }, []);
+    return allresult;
 }
 
+function getEmployeesCoverage(param) {
+  // seu código aqui
+  if (!param) {
+    return employeeLocation();
+  }
+  const findEmployee = data.employees.find((employee) => employee.id === param.id
+    || employee.firstName === param.name || employee.lastName === param.name);
+  if (!findEmployee) {
+    throw new Error('Informações inválidas', {
+      id: 'Id inválido' });
+  }
+  return employeeLocation().find((item) => item.id.includes(findEmployee.id));
+}
+console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
